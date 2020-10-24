022bb8e3bfa4fcf34f60135e999a75f7e2deb215
Jithin Nambiar <45201620+JithLord@users.noreply.github.com>
2020-10-24 22:06:37 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,5 @@
 from PIL import Image
 import string
-
     # 0 1-26 " " 27-36
     #   a-z  " "  0-9
 comb = [""]+[chr(x) for x in range(97,123)] + [str(i) for i in range(10)]    # [a:z] + [0:9]
@@ -23,38 +22,44 @@ def encode(im,text=""):
             if (len_text):      #To check if there's a character
                 val3 = int(next(dcoded))
                 len_text-=1
-            else:
-                val3 = 0
-            print('Char', val3, (r,g,b), '->', end="\t")
-            if val3>31:
-                print('Case 1', end="\t")
-                r = (r>>4<<4)  + 15
-                g = (g>>4<<4)  + 15
-                b = (b>>4<<4)  + val3 - 30
-            elif val3>15:
-                print('Case 2', end="\t")
-                r = (r>>4<<4)  + 15
-                g = (g>>4<<4)  + val3 - 15
-                b = (b>>4<<4)
-            else:
-                print('Case 3', end="\t")
-                r = (r>>4<<4)   + val3
-                g = (g>>4<<4)
-                b = (b>>4<<4)
-            print((r,g,b))
-            value = (r,g,b)
-            im.putpixel((x, y), value)
+                print('Char', val3, (r,g,b), '->', end="\t")
+                if val3>31:
+                    print('Case 1', end="\t")
+                    r = (r>>4<<4)  + 15
+                    g = (g>>4<<4)  + 15
+                    b = (b>>4<<4)  + val3 - 30
+                elif val3>15:
+                    print('Case 2', end="\t")
+                    r = (r>>4<<4)  + 15
+                    g = (g>>4<<4)  + val3 - 15
+                    b = (b>>4<<4)
+                else:
+                    print('Case 3', end="\t")
+                    r = (r>>4<<4)   + val3
+                    g = (g>>4<<4)
+                    b = (b>>4<<4)
+                print((r,g,b))
+                value = (r,g,b)
+                im.putpixel((x, y), value)
     im.show(im)
-    im.save("down.png")
+    im.save("down.jpg")
 
 
 def decode(im):
     width, height = im.size[0], im.size[1]
     text=""
+    print("Working")
     for x in range(height):
         for y in range(width):
             [r,g,b]=im.getpixel((x, y))
-            r,g,b = (r&15, g&15, b&15)
-            char = ecode.get(r+g+b)
+            print(r,g,b,end="\n")
+            r,g,b = r%240,g%240,b%240
+            char = ecode.get(r%240+g%240+b%240)
             print(char,end="")
             text+=char
+            if char!="":
+                break
+        else:
+            continue
+        break
+    # print(text,end="")
