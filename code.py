946f2b5895d029e6f36a7bada853bbaf5499c011
Jithin Nambiar <45201620+JithLord@users.noreply.github.com>
2020-10-24 20:19:38 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import string
+
     # 0 1-26 " " 27-36
     #   a-z  " "  0-9
 comb = [""]+[chr(x) for x in range(97,123)] + [str(i) for i in range(10)]    # [a:z] + [0:9]
@@ -15,18 +16,6 @@ def encode(im,text=""):
     print(dcoded)
     dcoded = iter(dcoded)
     len_text = len(text)
-
-    print('Before:')
-    r, g, b = im.getpixel((0, 0))
-    print(r, g, b)
-    r, g, b = im.getpixel((1, 1))
-    print(r, g, b)
-    r, g, b = im.getpixel((2, 2))
-    print(r, g, b)
-    r, g, b = im.getpixel((3, 3))
-    print(r, g, b)
-    r, g, b = im.getpixel((4, 4))
-    print(r, g, b)
 
     for x in range(height):
         for y in range(width):
@@ -55,19 +44,6 @@ def encode(im,text=""):
             print((r,g,b))
             value = (r,g,b)
             im.putpixel((x, y), value)
-    
-    print('After:')
-    r, g, b = im.getpixel((0, 0))
-    print(r, g, b)
-    r, g, b = im.getpixel((1, 1))
-    print(r, g, b)
-    r, g, b = im.getpixel((2, 2))
-    print(r, g, b)
-    r, g, b = im.getpixel((3, 3))
-    print(r, g, b)
-    r, g, b = im.getpixel((4, 4))
-    print(r, g, b)
-
     im.show(im)
     im.save("down.png")
 
@@ -75,35 +51,10 @@ def encode(im,text=""):
 def decode(im):
     width, height = im.size[0], im.size[1]
     text=""
-
-    print('Before:')
-    r, g, b = im.getpixel((0, 0))
-    print(r, g, b)
-    r, g, b = im.getpixel((1, 1))
-    print(r, g, b)
-    r, g, b = im.getpixel((2, 2))
-    print(r, g, b)
-    r, g, b = im.getpixel((3, 3))
-    print(r, g, b)
-    r, g, b = im.getpixel((4, 4))
-    print(r, g, b)
-
     for x in range(height):
         for y in range(width):
             [r,g,b]=im.getpixel((x, y))
-            # print((r,g,b), '->', end="\t")
-            # r,g,b = r%240,g%240,b%240
             r,g,b = (r&15, g&15, b&15)
-            # print((r,g,b),end="\n")
             char = ecode.get(r+g+b)
             print(char,end="")
             text+=char
-    # print(text)
-
-
-im = Image.open('download2.jpg')
-im.show(im)
-encode(im,"Jithin")
-print('\nDECODE:-----------------')
-im2 = Image.open('down.png')
-decode(im2)
