8b58d9114318121c853ecfbabdb25ea328804ca1
Jithin Nambiar <45201620+JithLord@users.noreply.github.com>
2020-11-07 23:34:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import image
+from code1 import decode, encode
 from PIL import Image
 
 im = Image.open('download2.jpg')
 im.show(im)
-image.encode(im,"qwertyuiopasdfghjklzxcvbnm1234567890")
+encode(im,"qwertyuiopasdfghjklzxcvbnm1234567890")
 print('\nDECODE:-----------------')
 im2 = Image.open('down.png')
-image.decode(im2)
+decode(im2)
