e4f96669f69df0e4826cb15a3a7b05fdacb251ef
sneha-l <73346512+sneha-l@users.noreply.github.com>
2020-12-05 23:15:10 +0530

    Update code1.py
    Improved image quality

--- code1.py
+++ code1.py
@@ -2,13 +2,15 @@ from PIL import Image
 import string
     # 0 1-26 " " 27-36
     #   a-z  " "  0-9
-comb = [""]+[chr(x) for x in range(97,123)] +[" "]+ [str(i) for i in range(10)]    # [a:z] + [0:9]
-numbers = [i for i in range(39)]                                      # [1:37]
+
+comb = [""]+[chr(x) for x in range(97,123)] +[" "]+ [str(i) for i in range(10)]    # [a:z] + [ ] + [0:9]
+numbers = [i for i in range(39)]                                      # [1:38]
 ecode = dict(zip(numbers,comb))                                         # {"1":"a", "2":"b",     }
 dcode = dict(zip(comb,numbers))                                         # {"a":"1", "b":"2",     }
- 
+
 
 def encode(im,text=""):
+    
     text = text.lower()
     width, height = im.size[0], im.size[1]
     dcoded = [dcode.get(i) for i in text]
@@ -16,42 +18,79 @@ def encode(im,text=""):
     dcoded = iter(dcoded)
     len_text = len(text)
 
-    for y in range(height):
-        for x in range(width):
-            [r,g,b]=im.getpixel((x, y))
-            if (len_text):      #To check if there's a character
+    for y in range(0,height):
+        for x in range(0,width,2):
+            [r1,g1,b1]=im.getpixel((x,y))
+            [r2,g2,b2]=im.getpixel((x+1,y))
+            if (len_text):      #To check if there's a character          
                 val3 = int(next(dcoded))
                 len_text-=1
             else:
                 val3 = 0
-            if val3>30:
-                r = (r>>4<<4)  + 15
-                g = (g>>4<<4)  + 15
-                b = (b>>4<<4)  + val3 - 30
-            elif val3>15:
-                r = (r>>4<<4)  + 15
-                g = (g>>4<<4)  + val3 - 15
-                b = (b>>4<<4)
+            if val3>35:
+                r1 = (r1>>3<<3) + 7
+                g1 = (g1>>3<<3) + 7
+                b1 = (b1>>3<<3) + 7
+                r2 = (r2>>3<<3) + 7
+                g2 = (g2>>3<<3) + 7
+                b2 = (b2>>3<<3) + val3 - 35
+            elif val3>28:
+                r1 = (r1>>3<<3) + 7
+                g1 = (g1>>3<<3) + 7
+                b1 = (b1>>3<<3) + 7
+                r2 = (r2>>3<<3) + 7
+                g2 = (g2>>3<<3) + val3 -28
+                b2 = (b2>>3<<3) 
+            elif val3>21:
+                r1 = (r1>>3<<3) + 7
+                g1 = (g1>>3<<3) + 7
+                b1 = (b1>>3<<3) + 7
+                r2 = (r2>>3<<3) + val3 - 21
+                g2 = (g2>>3<<3) 
+                b2 = (b2>>3<<3) 
+            elif val3>14:
+                r1 = (r1>>3<<3) 
+                g1 = (g1>>3<<3) 
+                b1 = (b1>>3<<3) 
+                r2 = (r2>>3<<3) + 7
+                g2 = (g2>>3<<3) + 7
+                b2 = (b2>>3<<3) + val3 - 14
+            elif val3>7:
+                r1 = (r1>>3<<3) 
+                g1 = (g1>>3<<3) 
+                b1 = (b1>>3<<3) 
+                r2 = (r2>>3<<3) + 7
+                g2 = (g2>>3<<3) + val3 - 7
+                b2 = (b2>>3<<3)
             elif val3>=0:
-                r = (r>>4<<4)   + val3
-                g = (g>>4<<4)
-                b = (b>>4<<4)
-            value = (r,g,b)
-            im.putpixel((x, y), value)
+                r1 = (r1>>3<<3) 
+                g1 = (g1>>3<<3) 
+                b1 = (b1>>3<<3) 
+                r2 = (r2>>3<<3) + val3
+                g2 = (g2>>3<<3) 
+                b2 = (b2>>3<<3) 
+            value1 = (r1,g1,b1)
+            im.putpixel((x, y), value1)
+            value2 = (r2,g2,b2)
+            im.putpixel((x+1, y), value2)
     #im.show(im)
-    #im.save("down.png")
+    im.save("down.png")
     return im
 
 
 def decode(im):
+
     width, height = im.size[0], im.size[1]
     text=""
-    for y in range(height):
-        for x in range(width):
-            [r,g,b]=im.getpixel((x, y))
-            r,g,b = (r&15, g&15, b&15)
-            char = ecode.get(r+g+b)
-            print(char,end="")
+
+    for y in range(0,height):
+        for x in range(0,width,2):
+            [r1,g1,b1]=im.getpixel((x, y))
+            [r2,g2,b2]=im.getpixel((x+1, y))
+            r1,g1,b1 = (r1&7, g1&7, b1&7)
+            r2,g2,b2 = (r2&7, g2&7, b2&7)
+            char = ecode.get(r1+g1+b1+r2+g2+b2)
+            #print(char,end="")
             text+=char
-
-
+    return text,im
+    
